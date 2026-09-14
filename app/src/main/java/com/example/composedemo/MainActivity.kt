a0027fd6d57a47d3456c9a5ca922e732c46af3ab
Anton <antonbaturinskiy261@gmail.com>
2026-09-14 14:49:05 +0700

    Add DemoText and DemoSlider

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -7,10 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +48,28 @@ fun GreetingPreview() {
     ComposeDemoTheme {
         Greeting("Android")
     }
+}
+
+// ⬇⬇⬇ ДОБАВЛЯЕМ ЭТИ ДВЕ ФУНКЦИИ ⬇⬇⬇
+
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun DemoSlider(
+    sliderPosition: Float,
+    onPositionChange: (Float) -> Unit
+) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) }
+    )
 }
